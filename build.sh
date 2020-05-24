274253b52ea317cea6a065901a5cf396ec534a3f
Julin S <48789920+ju-sh@users.noreply.github.com>
2020-05-24 16:31:24 +0530

    add .travis.yml

--- build.sh
+++ build.sh
@@ -1,7 +1,12 @@
 rm -rf build/ dist/ src/tzview.egg-info/
 
 python3 setup.py sdist bdist_wheel
+
+# For uploading new package
 # python3 -m twine upload --repository-url https://upload.pypi.org/legacy/ dist/*
+
+# For modifying package that was already uploaded
+# python3 -m twine upload --skip-existing dist/*
 
 # Change version number in:
 #  - src/tzview/app.py:__version__ = "0.1"
